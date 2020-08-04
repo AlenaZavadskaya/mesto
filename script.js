cd559d32c2c4f625f4962aeb3a7b5a3b7fe1fa16
AlenaZavadskaya <helen.zavadskaya@yandex.ru>
2020-08-04 09:57:48 +0300

    Сохранить изменения в форме

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-/*console.log('hello');
-let content = document.querySelector('.content');
-let profile = content.querySelector('.profile');
-
-let profileHeader = content.querySelector('.profile__header');
-let profileParagraph = content.querySelector('.profile__paragraph');
-
-console.log(profileHeader);
-console.log(profileParagraph);*/
-
 let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.edit-button');
 let popupCloseButton = popup.querySelector('.popup__close');
+let submitButton = popup.querySelector('.submit__button');
+let page = document.querySelector('.page');
+// Находим форму в DOM
+let formElement = document.querySelector('.form');
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.form__item_name');
+let jobInput = document.querySelector('.form__item_about');
+// Выберите элементы, куда должны быть вставлены значения полей
+let placeNameInput = document.querySelector('.profile__name');
+let placeJobInput = document.querySelector('.profile__about');
 
+
+// открыть попап
 let popupToggle = function (event) {
 	event.preventDefault();
 	popup.classList.toggle('popup_opened');
 }
-/*popupToggle();*/
 
+// закрыть попап
 let closePopup = function (event) {
 	if (event.target !== event.currentTarget) return
 	popupToggle(event);
@@ -28,38 +30,21 @@ popupCloseButton.addEventListener('click', popupToggle);
 popup.addEventListener('click', closePopup);
 
 
-let page = document.querySelector('.page');
-
-
-
-// Находим форму в DOM
-let formElement = popup.querySelector('.form')
-
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-    let nameInput = form.querySelector('.form__item_name')
-    let jobInput = form.querySelector('.form__item_about')
-
+	
 	 // Получите значение полей из свойства value
 	let name = nameInput.value;
 	let job = jobInput.value;
 
-    // Выберите элементы, куда должны быть вставлены значения полей
-	let placeNameInput = document.querySelector('.profile__name');
-	let placeJobInput = content.querySelector('.profile__about');
-	
-	
 	 // Вставьте новые значения с помощью textContent
-	placeNameInput.textContent = name;
-	placeJobInput.textContent = job;
+	placeNameInput.textContent = nameInput.value;
+	placeJobInput.textContent = jobInput.value;
+	popupToggle(event);
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('.submit__button', formSubmitHandler);
+formElement.addEventListener('submit', formSubmitHandler);
