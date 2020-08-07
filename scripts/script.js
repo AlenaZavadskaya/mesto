33aa1fa6a93d09a3c2de51ee6bc8e4df24ee4fe0
AlenaZavadskaya <helen.zavadskaya@yandex.ru>
2020-08-07 22:19:07 +0300

    на ревью 2

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+let popup = document.querySelector('.popup'); 
+let popupOpenButton = document.querySelector('.edit-button'); 
+let popupCloseButton = popup.querySelector('.popup__close'); 
+let submitButton = popup.querySelector('.submit__button'); 
+let page = document.querySelector('.root'); 
+let formElement = document.querySelector('.form'); 
+let nameInput = document.querySelector('.form__item_name'); 
+let jobInput = document.querySelector('.form__item_about'); 
+let placeNameInput = document.querySelector('.profile__name'); 
+let placeJobInput = document.querySelector('.profile__about'); 
+
+
+// открыть попап 
+let popupToggle = function (event) { 
+	event.preventDefault(); 
+	popup.classList.toggle('popup_opened');
+	nameInput.value = placeNameInput.textContent; 
+	jobInput.value = placeJobInput.textContent; 
+} 
+
+// закрыть попап 
+let closePopup = function (event) { 
+	if (event.target !== event.currentTarget) return
+	popupToggle(event); 
+} 
+
+popupOpenButton.addEventListener('click', popupToggle); 
+popupCloseButton.addEventListener('click', popupToggle); 
+popup.addEventListener('click', closePopup); 
+
+
+// Обработчик «отправки» формы, хотя пока 
+// она никуда отправляться не будет 
+function formSubmitHandler (evt) { 
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. 
+
+	 // Вставьте новые значения с помощью textContent 
+	placeNameInput.textContent = nameInput.value; 
+	placeJobInput.textContent = jobInput.value; 
+	popupToggle(event); 
+} 
+
+// Прикрепляем обработчик к форме: 
+// он будет следить за событием “submit” - «отправка» 
+formElement.addEventListener('submit', formSubmitHandler);
